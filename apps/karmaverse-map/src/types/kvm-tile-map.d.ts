6382a36f437b654596313b5ad1bc6ee6f3811398
Max.li <maxli6688@qq.com>
2025-10-19 01:06:11 +0800

    fix: build error

--- apps/karmaverse-map/src/types/kvm-tile-map.d.ts
+++ apps/karmaverse-map/src/types/kvm-tile-map.d.ts
@@ -1,1 +1,21 @@
-declare module '@karmaverse/kvm-tile-map';
+declare module '@karmaverse/kvm-tile-map' {
+  export interface Coord {
+    x: number;
+    y: number;
+  }
+
+  export interface Layer {
+    (x: number, y: number): any;
+  }
+
+  export interface TileMapProps {
+    [key: string]: any;
+  }
+
+  export interface TileMapLite {
+    [key: string]: any;
+  }
+
+  export const TileMap: any;
+  export const TileMapLite: any;
+}
